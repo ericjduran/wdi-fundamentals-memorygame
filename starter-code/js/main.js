1f3b72a83aea0797cde50b0e7e36f615970720d5
ericjduran <ericduran247@gmail.com>
2017-03-26 11:02:11 -0400

    wii-fundamentals-memorygame
    GA memory game

---
+++ starter-code/js/main.js
@@ -0,0 +1,43 @@
+Eric Duran [10:02 PM] 
+var cards = [“queen”, “king”, “queen”, “king”];
+
+var cardsInPlay = [];
+
+var gameBoard = document.getElementById(“game-board”);
+
+var createCards = function (){
+    for (var i = 0; i < cards.length; i++) {
+        var newCards = document.createElement(‘div’)
+        newCards.className = ‘card’;
+
+        newCards.setAttribute(‘data-card’,cards[i]);
+
+        gameBoard.appendChild(newCards);
+
+        newCards.addEventListener(‘click’, isTwoCards);
+    }
+};
+
+
+
+function isTwoCards(){
+
+    cardsInPlay.push(this.getAttribute(‘data-card’));
+
+    if(this.getAttribute(‘data-card’) === “king”) {
+        this.innerHTML = ‘<img src=“images-folder/king.png”>’;
+    } else if (this.getAttribute(‘data-card’) === “queen”){
+        this.innerHTML = ‘<img src=“images-folder/queen.png”>’;
+    }
+
+    if (cardsInPlay.length === 2) {
+
+        if (cardsInPlay[0] === cardsInPlay[1]) {
+            alert(“Is a Match!“);
+        } else {
+            alert(“Try Again”);
+               cardsInPlay = [];
+        }
+    }
+}
+createCards();
